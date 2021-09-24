aba8203e318c843f76341a3a0f45f4df6496477d
annAleighh <sourlemon2270@gmail.com>
2021-09-24 16:20:34 -0400

    pwogaming

--- blackJack/script.js
+++ blackJack/script.js
@@ -4,6 +4,7 @@ const ranks = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven",
 let deck = buildDeck();
 let playerHand = [];
 let houseHand = [];
+let playing = true;
 
 function buildDeck() {
     let deck = [];
@@ -26,63 +27,61 @@ function randCard(deck) {
 }
 
 function dealToHand(hand, deck, numOfCards, player) {
-    let card = [];
     for (let i = 0; i < numOfCards; i++) {
-        card = [randCard(deck)];
-        hand.push(card[0]);
-        displayHand(card, player);
+        let card = randCard(deck);
+        hand.push(card);
     }
-    console.log(handValue(playerHand));
-}
-
-function dealToHandInitial(hand, deck, numOfCards, player) {
-    let card = [];
-    for (let i = 1; i < numOfCards; i++) {
-        card = [randCard(deck)];
-        hand.push(card[0]);
-        displayHand(card, player);
-    }
+    displayHand(hand, player);
     console.log(handValue(playerHand));
 }
 
 function setCard(card, id, firstCard) {
     let cardEl = document.createElement("img");
-    let backCard = document.createElement("img");
     let cards = document.getElementById(id);
     if (firstCard === true) {
-        cardEl.style.backfaceVisibility = "hidden";
-        cardEl.style.transform = "rotateY(180deg)";
-        backCard.setAttribute("src", "images/backOfCard.png");
-        backCard.setAttribute("alt", "back of card");
-        backCard.setAttribute("style", "width:125px;");
-        backCard.setAttribute("style", "height:175px;");
-        cards.appendChild(backCard);
-        return backCard;
+        cardEl.setAttribute("src", "images/backOfCard.png");
+        cardEl.setAttribute("alt", "back of card");
     }
     else {
         cardEl.setAttribute("src", ("images/cards/" + card.rank + "Of" + card.suit + ".png"));
         cardEl.setAttribute("alt", card.rank + " of " + card.suit);
-        cardEl.setAttribute("style", "width:125px;");
-        cardEl.setAttribute("style", "height:175px;");
-        cards.appendChild(cardEl);
-        return cardEl;
     }
+    cardEl.setAttribute("style", "width:125px;");
+    cardEl.setAttribute("style", "height:175px;");
+    cards.appendChild(cardEl);
+    return cardEl;
 }
 
-// create displayCard function
+// create displayCard functionS
 
-function displayHand(hand, person) {
+function displayHand(hand, person, showAllCards) {
     if (person === "player") {
         id = "playerHand";
     }
     else {
         id = "houseHand";
     }
+    let personEl = document.getElementById(id);
+    removeChild(document.getElementById(id));
     console.log(id);
-    // HELP WHY HE NO WORK
-    setCard(hand[0], id, true);
-    for (let i = 1; i <= hand.length; i++) {
-        setCard(hand[i], id, false);
+    // HELP WHY HE NO WORKS
+    if (!showAllCards) {
+        setCard(hand[0], id, id === "houseHand");
+        for (let i = 1; i < hand.length; i++) {
+            setCard(hand[i], id, false);
+        }
+    }
+    else {
+        for (let i = 0; i < hand.length; i++) {
+            setCard(hand[i], id, false);
+        }
+    }
+
+    if (handValue(hand) > 21) {
+        personEl.style.backgroundColor = "red";
+    }
+    else if (handValue(hand) === 21) {
+        personEl.style.backgroundColor = "rgb(217, 255, 0)";
     }
 }
 //after get this working make card flip back over and remove back of card image in endgame
@@ -109,7 +108,7 @@ function handValue(hand) {
 }
 
 function housePlay() {
-    if (handValue(houseHand) < 15) {
+    if (handValue(houseHand) < 17) {
         dealToHand(houseHand, deck, 1, "house");
     }
 }
@@ -122,27 +121,48 @@ function addParagraph(place, msg) {
 }
 
 function endGame() {
-    housePlay();
-    let houseScore = handValue(houseHand);
-    let playerScore = handValue(playerHand);
-    let win = false;
-    // should there be different messages?  
-    if (playerScore <= 21 && (playerScore > houseScore || houseScore > 21)) {
-        addParagraph("result", " Win! ");
-    }
-    else if (houseScore <= 21 && (houseScore > playerScore || playerScore > 21)) {
-        addParagraph("result", " loss :( ");
-    }
-    else if (houseScore === playerScore || (houseScore > 21 && playerScore > 21)) {
-        let x = Math.floor(Math.random() * 2)
-        // why not:  win = x > 1;
-        if (x > 1) {
-            addParagraph("result", " Win! ");
+    let body = document.body;
+    
+    if (playing === true) {
+        let playerScore = handValue(playerHand);
+        if (playerScore < 21) {
+            housePlay();
+        }
+        let houseScore = handValue(houseHand);
+        displayHand(houseHand, "house", true);
+        // should there be different messages?
+        if (playerScore <= 21 && (playerScore > houseScore || houseScore > 21)) {
+            addParagraph("result", " Win !! ");
+            body.style.backgroundColor = "rgb(159, 252, 159)";
+            playing = false;
+        }
+        else if (houseScore <= 21 && houseScore > playerScore) {
+            addParagraph("result", " loss :( ");
+            body.style.backgroundColor = "rgb(253, 122, 129)";
+            playing = false;
+        }
+        else if (houseScore <= 21 && playerScore > 21) {
+            addParagraph("result", " bust D: ");
+            body.style.backgroundColor = "rgb(122, 168, 253)";
+            playing = false;
+        }
+        else if (houseScore === playerScore) {
+            addParagraph("result", " push :/ ");
+            body.style.backgroundColor = "rgb(216, 122, 253)";
+            playing = false;
         }
         else {
-            addParagraph("result", " loss :( ");
+            addParagraph("result", " ERROR ");
+            body.style.backgroundColor = "rgb(255, 255, 255)";
+            playing = false;
         }
     }
+    else {
+        console.log("oops");
+    }
+
+    button = document.getElementById("playAgain");
+    button.style.visibility = "visible";
 }
 
 function playAgain() {
@@ -154,10 +174,16 @@ function resetUI() {
     let playerHand = document.getElementById("playerHand");
     let houseHand = document.getElementById("houseHand");
     let result = document.getElementById("result");
+    let button = document.getElementById("playAgain");
+    let body = document.body;
     removeChild(playerHand);
     removeChild(houseHand);
     removeChild(result);
-    console.log("UI RESET, CODE RED");
+    console.log("UI RESET, CODE YELLOW");
+    button.style.visibility = "hidden";
+    body.style.backgroundColor = "rgb(255, 225, 142)";
+    playerHand.style.backgroundColor = "orange";
+    houseHand.style.backgroundColor = "orange";
 }
 
 
@@ -174,19 +200,28 @@ function resetLogic() {
     houseHand = [];
     dealToHand(playerHand, deck, 2, "player");
     dealToHand(houseHand, deck, 2, "dealer");
+    playing = true;
 }
 
 function hit() {
-    dealToHand(playerHand, deck, 1, "player");
-    if (handValue(playerHand) > 21) {
+    if (playing === true) {
+        dealToHand(playerHand, deck, 1, "player");
+        if (handValue(playerHand) >= 21) {
+            endGame();
+        }
+    }
+}
+
+function earlyWinner() {
+    if (handValue(playerHand) === 21 || handValue(houseHand) === 21) {
         endGame();
     }
 }
 
 
 
-dealToHand(playerHand, deck, 2, "player");
-dealToHand(houseHand, deck, 2, "dealer");
+playAgain();
+earlyWinner();
 console.log(playerHand);
 console.log(houseHand);
 console.log(handValue(playerHand));
