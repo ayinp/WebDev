681a44c86fec73f87ef6e57e83a62780cc630356
annAleighh <sourlemon2270@gmail.com>
2021-12-12 13:26:44 -0500

    woo hes done other than style

--- eventClient/server.js
+++ eventClient/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const pug = require ('pug');
+const pug = require('pug');
 // const { nanoid } = require('nanoid');
 const app = express();
 const port = 7000;
@@ -8,7 +8,7 @@ const { send, allowedNodeEnvironmentFlags } = require('process');
 var session = require('express-session');
 const axios = require('axios')
 axios.defaults.baseURL = 'http://localhost:3000';
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(session({
     secret: 'no <3',
@@ -19,131 +19,139 @@ app.use(session({
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get("/pugtest", (req, res) => {
-    axios.get('/events')
-    .then(responce => {
-        let theEvent = 
-        res.render("pug", {name: "greg :/", stuff: responce.data});
-    })
-
-})
-
-app.use(function (req, res, next){
-    if(req.path === "/loginStyle.css" || req.path === "/favicon.ico" || req.path === "/script.js"|| req.path === "/views/pug.pug"){
+app.use(function (req, res, next) {
+    if (req.path === "/loginStyle.css" || req.path === "/favicon.ico" || req.path === "/script.js" || req.path === "/views/pug.pug") {
         next();
         return;
     }
-    if(req.path !== "/login.html" && req.session.username === undefined){
+    if (req.path !== "/login.html" && req.session.username === undefined) {
         res.redirect("/login.html");
         return;
     }
     next();
 })
 
+app.get("/createEvent", (req, res) => {
+    res.render("createEvent");
+})
+
+app.post("/createEvent", (req, res) => {
+    axios.post('/events', req.body)
+        .then(response => {
+            console.log(response);
+            res.redirect('/index.html');
+        })
+        .catch(error => {
+            console.log("oops #555555555");
+            console.log(error);
+            res.status(400).json(":(");
+        })
+})
+
 app.post('/signups', (req, res) => {
-    axios.post('/signups', {studentId: req.session.studentId, eventId: req.body.eventId, signIn: null, signOut: null})
-    .then(response => {
-        console.log(response);
-        res.json({status: "ok ig :/"});
-    })
-    .catch(error => {
-        console.log("oops");
-        console.log(error);
-        res.status(400).json("urgle urgle :( \n" + error);
-    })
+    axios.post('/signups', { studentId: req.session.studentId, eventId: req.body.eventId, signIn: null, signOut: null })
+        .then(response => {
+            console.log(response);
+            res.json({ status: "ok ig :/" });
+        })
+        .catch(error => {
+            console.log("oops");
+            console.log(error);
+            res.status(400).json("urgle urgle :( \n" + error);
+        })
 })
 
 app.patch('/signIn', (req, res) => {
     let signUp = req.session.myEvents.find(event => event.id === req.body.eventId)
-    axios.patch('/signups/' + signUp.signupId, {sign_in: "yes"})
-    .then(responce => {
-        res.status(200).json(responce.data)
-    })
-    .catch(error => {
-        console.log("oops");
-        console.log(error);
-        res.status(400)
-    })
+    axios.patch('/signups/' + signUp.signupId, { sign_in: "yes" })
+        .then(responce => {
+            res.status(200).json(responce.data)
+        })
+        .catch(error => {
+            console.log("oops");
+            console.log(error);
+            res.status(400)
+        })
 })
 
 app.patch('/signOut', (req, res) => {
     console.log("someones trying to patch me !!! D:")
     let signUp = req.session.myEvents.find(event => event.id === req.body.eventId)
-    axios.patch('/signups/' + signUp.signupId, {sign_out: "yes"})
-    .then(responce => {
-        res.status(200).json(responce.data)
-    })
-    .catch(error => {
-        console.log("oops");
-        console.log(error);
-        res.status(400)
-    })
+    axios.patch('/signups/' + signUp.signupId, { sign_out: "yes" })
+        .then(responce => {
+            res.status(200).json(responce.data)
+        })
+        .catch(error => {
+            console.log("oops");
+            console.log(error);
+            res.status(400)
+        })
 })
 
 app.post('/signUp/:id', (req, res) => {
     axios.patch('/signups/:id', req.body)
-    .then(response => {
-        console.log(response);
-        res.json({status: "ok ig :/"});
-    })
-    .catch(error => {
-        console.log("oops");
-        console.log(error);
-        res.status(400).json(error);
-    })
+        .then(response => {
+            console.log(response.data);
+            res.json({ status: "ok ig :/" });
+        })
+        .catch(error => {
+            console.log("oops");
+            console.log(error);
+            res.status(400).json(error);
+        })
 })
 
 app.get('/myEvents', (req, res) => {
     axios.get('/students/' + req.session.studentId + '/events')
-    .then(response => {
-        console.log(response.data);
-        req.session.myEvents = response.data;
-        res.json(response.data).status(200);
-    })
-    .catch(error => {
-        console.log("oops");
-        console.log(error);
-    })
+        .then(response => {
+            console.log(response.data);
+            req.session.myEvents = response.data;
+            res.json(response.data).status(200);
+        })
+        .catch(error => {
+            console.log("oops");
+            console.log(error);
+        })
 })
 
 // make this for events and signups too
-app.post('/login.html', (req, res) =>{
+app.post('/login.html', (req, res) => {
     axios.get('/students')
-    .then(response => {
-        console.log("hi studies");
-        let s = response.data.find(student => makeUsername(student) === req.body.username)
-        if(s){
-            console.log("found studie!! :)");
-            console.log(s);
-            req.session.username = makeUsername(s);
-            req.session.studentId = s.id;
-            res.redirect("/");
-        }
-        else{
-            console.log("no studie :(");
+        .then(response => {
+            console.log("hi studies");
+            let s = response.data.find(student => makeUsername(student) === req.body.username)
+            if (s) {
+                console.log("found studie!! :)");
+                console.log(s);
+                req.session.username = makeUsername(s);
+                req.session.studentId = s.id;
+                res.redirect("/");
+            }
+            else {
+                console.log("no studie :(");
+                res.redirect("/login.html");
+            }
+        })
+        .catch(error => {
+            console.log("oops");
+            console.log(error);
             res.redirect("/login.html");
-        }
-    })
-    .catch(error => {
-        console.log("oops");
-        console.log(error);
-        res.redirect("/login.html");
-    })
+        })
 })
 
 app.get('/events', (req, res) => {
     axios.get('/events')
-    .then(response => {
-        res.json(response.data);
-    })
-    .catch(error => {
-        console.log("oops");
-        console.log(error);
-    })
+        .then(response => {
+            res.json(response.data);
+        })
+        .catch(error => {
+            console.log("oops");
+            console.log(error);
+        })
 })
 
 
-function makeUsername(student){
+function makeUsername(student) {
     return student.firstname + student.lastname;
 }
 
